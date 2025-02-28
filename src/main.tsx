1bd97814cfcaae8dc4e41bbe52f19571d0876672
吉田 健太郎 <ken_yoshida@zeus.mcinc.local>
2025-02-28 10:46:28 +0900

    added authenticator

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,9 @@ import ReactDOM from "react-dom/client";
 import { Authenticator } from '@aws-amplify/ui-react';
 import App from "./App.tsx";
 import "./index.css";
-import '@aws-amplify/ui-react/styles.css';
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
+import '@aws-amplify/ui-react/styles.css';
 
 Amplify.configure(outputs);
 
