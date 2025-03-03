d605ecc037e6fa531dedebf2c816a45dae5a3876
吉田 健太郎 <ken_yoshida@zeus.mcinc.local>
2025-03-03 13:18:00 +0900

    test2

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,29 @@
-import { Authenticator } from '@aws-amplify/ui-react'
-import '@aws-amplify/ui-react/styles.css'
 import { useEffect, useState } from "react";
-import { StorageBrowser } from '@aws-amplify/ui-react-storage';
+import type { Schema } from "../amplify/data/resource";
+import { generateClient } from "aws-amplify/data";
 
 const client = generateClient<Schema>();
 
 function App() {
-  const defaultPrefixes = [
-    'dev-bedrock-s3-us/web',
-  ];
+  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+
+  useEffect(() => {
+    client.models.Todo.create({ content: window.prompt("Todo content") });
+  }
 
   return (
-    <Authenticator>
-      {({ signOut }) => (
-        <StorageBrowser defaultPrefixes={defaultPrefixes} />
-      )}
-    </Authenticator>
+    <main>
+      <h1>My todos</h1>
+      <button onClick={createTodo}>+ new</button>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.content}</li>
+        ))}
+      </ul>
+      <div>
+          Review next step of this tutorial.
+        </a>
+      </div>
+    </main>
   );
 }
-
-export default App;
